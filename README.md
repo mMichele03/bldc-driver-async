29d72cf6f5088f043c89a767cbb8b157c5bab909
Mikle <m.michele.2003@gmail.com>
2026-07-24 19:50:26 +0200

    test: spi encoder read test
    Co-authored-by: NightBerserker <antonio.lamontagna@mail.polimi.it>

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 [memory.x](https://github.com/embassy-rs/embassy/blob/main/examples/rp235x/memory.x)
 
 [EXAMPLES](https://github.com/embassy-rs/embassy/tree/main/examples/rp235x/src/bin)
+
+[AS5048A magnetic encoder datasheet](https://www.mouser.com/datasheet/2/588/AS5048_DS000298_4-00-1100510.pdf?srsltid=AfmBOopVQXd4zM0YdcdOcmUoEoGPYkSAAHg5qVXJu2K3LdYZ0NbKSO3k)
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ use embassy_executor::Spawner;
 use embassy_rp::bind_interrupts;
 use embassy_rp::gpio::{Level, Output};
 use embassy_rp::peripherals::USB;
+use embassy_rp::spi::{Config, Spi};
 use embassy_rp::usb::{Driver, InterruptHandler};
 use embassy_time::Timer;
+use log::info;
 use panic_probe as _;
 
 bind_interrupts!(struct Irqs {
@@ -18,25 +20,53 @@ async fn logger_task(driver: Driver<'static, USB>) {
     embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver);
 }
 
+// Testing AS5048A SPI magnetic encoder
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) -> ! {
     let p = embassy_rp::init(Default::default());
-    let mut led = Output::new(p.PIN_25, Level::Low);
+    // let mut led = Output::new(p.PIN_25, Level::Low);
 
     let driver = Driver::new(p.USB, Irqs);
     spawner.spawn(logger_task(driver)).unwrap();
 
+    let clk = p.PIN_2;
+    let mosi = p.PIN_3;
+    let miso = p.PIN_4;
+    let mut cs = Output::new(p.PIN_5, Level::High);
+
+    let mut spi_config = Config::default();
+    spi_config.frequency = 10_000_000; // max 10 MHz (10_000_000)
+    spi_config.phase = embassy_rp::spi::Phase::CaptureOnSecondTransition;
+
+    let mut spi = Spi::new(p.SPI0, clk, mosi, miso, p.DMA_CH0, p.DMA_CH1, spi_config);
+
+    info!("Hello World!");
+
     let mut counter = 0;
     loop {
         counter += 1;
+        let buf_tx = [0xffu8; 2];
+        let mut buf_rx = [0xffu8; 2];
 
-        log::info!("counter: {}", counter);
+        cs.set_low();
+        spi.transfer(&mut buf_rx, &buf_tx).await.unwrap();
+        cs.set_high();
 
-        led.set_level(if counter % 2 == 0 {
-            Level::High
-        } else {
-            Level::Low
-        });
-        Timer::after_millis(1000).await;
+        let parity = (buf_rx[0] & 0x80) >> 7;
+        let error = (buf_rx[0] & 0x40) >> 6;
+        let angle_raw = ((buf_rx[0] & 0x3f) as u16) << 8 | (buf_rx[1] as u16);
+        let angle: f32 = angle_raw as f32 / 16383.0 * 360.0;
+
+        log::info!(
+            "tick {} [PAR {} ERR {}] raw {} angle {}",
+            counter,
+            parity,
+            error,
+            angle_raw,
+            angle
+        );
+
+        Timer::after_millis(10).await;
     }
 }
