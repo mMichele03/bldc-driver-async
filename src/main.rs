fb630923e321e343a04a2589eb6326c6d922ce73
Mikle <m.michele.2003@gmail.com>
2026-07-24 18:44:22 +0200

    test: serial print test working
    Co-authored-by: NightBerserker <antonio.lamontagna@mail.polimi.it>

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,35 @@
 #![no_main]
 
 use embassy_executor::Spawner;
+use embassy_rp::bind_interrupts;
 use embassy_rp::gpio::{Level, Output};
+use embassy_rp::peripherals::USB;
+use embassy_rp::usb::{Driver, InterruptHandler};
 use embassy_time::Timer;
 use panic_probe as _;
 
+bind_interrupts!(struct Irqs {
+    USBCTRL_IRQ => InterruptHandler<USB>;
+});
+
+#[embassy_executor::task]
+async fn logger_task(driver: Driver<'static, USB>) {
+    embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver);
+}
+
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) -> ! {
+async fn main(spawner: Spawner) -> ! {
     let p = embassy_rp::init(Default::default());
     let mut led = Output::new(p.PIN_25, Level::Low);
+
+    let driver = Driver::new(p.USB, Irqs);
+    spawner.spawn(logger_task(driver)).unwrap();
 
     let mut counter = 0;
     loop {
         counter += 1;
+
+        log::info!("counter: {}", counter);
 
         led.set_level(if counter % 2 == 0 {
             Level::High
