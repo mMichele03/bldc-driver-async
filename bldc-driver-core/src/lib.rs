c3c0c12e65e656a6815cc166a2585a1c4b4c7c03
Mikle <m.michele.2003@gmail.com>
2026-08-22 19:17:58 +0200

    feat(core): telemetry logic done
    Co-authored-by: NightBerserker <antonio.lamontagna@mail.polimi.it>

--- bldc-driver-core/src/lib.rs
+++ bldc-driver-core/src/lib.rs
@@ -1,3 +1,3 @@
 #![no_std]
 
-mod telemetry;
+pub mod telemetry;
--- bldc-driver-core/src/telemetry.rs
+++ bldc-driver-core/src/telemetry.rs
@@ -1,8 +1,15 @@
-use bldc_driver_hal::{self, EncoderData};
+use bldc_driver_hal::IntAngle;
+use bldc_driver_hal::TelemetryFlash;
+use embassy_time::Instant;
 use embassy_time::Timer;
 use heapless::Vec;
 
-pub struct TelemetryData {}
+pub struct TelemetryData<const BITS: usize> {
+    estimated_angle: IntAngle<BITS>,
+    measured_angle: IntAngle<BITS>,
+    timestamp: Instant,
+    speed_rpm: i32,
+}
 
 /// Telemetry task loop, intended to be run in an embassy task
 ///
@@ -15,18 +22,28 @@ pub struct TelemetryData {}
 ///     // handle telemetry end...
 /// }
 /// ```
-async fn telemetry_run<const ENCODER_BITS: usize, const BUFFER_SIZE: usize>(
+pub async fn telemetry_run<const ENCODER_BITS: usize, const BUFFER_SIZE: usize>(
     period_us: u64,
     mut rx: bldc_driver_hal::EncoderReceiver<ENCODER_BITS>,
+    mut flash: impl TelemetryFlash<TelemetryData<ENCODER_BITS>, BUFFER_SIZE>,
 ) {
-    let mut buffer: Vec<EncoderData<ENCODER_BITS>, BUFFER_SIZE> = Vec::new();
+    let mut buffer: Vec<TelemetryData<ENCODER_BITS>, BUFFER_SIZE> = Vec::new();
 
     loop {
         if let Some(reading) = rx.try_get() {
-            if buffer.push(reading).is_err() {
+            let data = TelemetryData {
+                estimated_angle: IntAngle::new(0),
+                measured_angle: reading.angle,
+                timestamp: Instant::now(),
+                speed_rpm: 0,
+            };
+            if buffer.push(data).is_err() {
                 break;
             }
         }
+
         Timer::after_micros(period_us).await;
     }
+
+    flash.write_data_vec(buffer);
 }
