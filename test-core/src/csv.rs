2280300ae9cd581ebdc049b4096061ca57d43932
Mikle <m.michele.2003@gmail.com>
2026-08-30 14:08:20 +0200

    feat(test): add velocity as deg_s in csv, remove kill plots from script
    Co-authored-by: NightBerserker <antonio.lamontagna@mail.polimi.it>

--- test-core/src/csv.rs
+++ test-core/src/csv.rs
@@ -13,20 +13,21 @@ pub fn write_to_csv(data: Vec<TestKinEstData>, filename: &str) -> Result<(), Err
     // Write the CSV header
     writeln!(
         wtr,
-        "timestamp,angle,angle_deg,est_angle,est_angle_deg,est_velocity"
+        "timestamp,angle,angle_deg,est_angle,est_angle_deg,est_velocity,est_velocity_deg_s"
     )?;
 
     // Write each row
     for row in data {
         writeln!(
             wtr,
-            "{},{},{:.2},{},{:.2},{}",
+            "{},{},{:.2},{},{:.2},{},{:.2}",
             row.timestamp,
             row.angle.raw_value(),
             row.angle.raw_value() as f32 * 360.0 / Angle::MAX_VALUE as f32,
             row.est_angle.raw_value(),
             row.est_angle.raw_value() as f32 * 360.0 / Angle::MAX_VALUE as f32,
-            row.est_velocity
+            row.est_velocity,
+            row.est_velocity as f32 * 360.0 / Angle::MAX_VALUE as f32,
         )?;
     }
 
