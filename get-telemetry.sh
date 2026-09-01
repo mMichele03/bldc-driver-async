254b3afda12d1abcd59880f72fc14e3f58676950
Mikle <m.michele.2003@gmail.com>
2026-09-01 21:34:02 +0200

    chore: fix telemetry plot script to handle any file
    Co-authored-by: NightBerserker <antonio.lamontagna@mail.polimi.it>

--- get-telemetry.sh
+++ get-telemetry.sh
@@ -1,3 +1,3 @@
 sudo picotool save -r 0x10100000 0x101249F0 telemetry.bin
 sudo python3 tools/parse_telemetry.py telemetry.bin telemetry_parsed.csv
-./telemetry-plot.sh
+./telemetry-plot.sh telemetry_parsed.csv
--- telemetry-plot.sh
+++ telemetry-plot.sh
@@ -1,5 +1,5 @@
 # pkill gnuplot
-gnuplot -p -e 'set datafile separator ","; plot "telemetry_parsed.csv" using 1:2 with lines, "" using 1:3 with lines'
-gnuplot -p -e 'set datafile separator ","; plot "telemetry_parsed.csv" using 1:4 with lines'
-gnuplot -p -e 'set datafile separator ","; plot "telemetry_parsed.csv" using 1:5 with lines, "" using 1:6 with lines, "" using 1:7 with lines'
-# gnuplot -p -e 'set datafile separator ","; plot "telemetry_parsed.csv" using 1:3 with lines'
+gnuplot -p -e "set datafile separator ','; plot '$1' using 1:2 with lines, '' using 1:3 with lines"
+gnuplot -p -e "set datafile separator ','; plot '$1' using 1:4 with lines"
+# gnuplot -p -e "set datafile separator ','; plot '$1' using 1:5 with lines, '' using 1:6 with lines, '' using 1:7 with lines"
+# gnuplot -p -e "set datafile separator ','; plot '$1' using 1:3 with lines"
